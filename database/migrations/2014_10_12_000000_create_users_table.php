6b17fd80fc2ba599a55f4f999ba7358d201c86bc
Eduardo Domingos <eduardoaparecido461@gmail.com>
2023-01-09 22:40:03 -0300

    feat: implementação da migrate/model User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('access_level')->default(0)->comment('Nível de acesso 0 = usuário comum e 1 = administrador');
             $table->rememberToken();
             $table->timestamps();
         });
